96ab7da14ade6872a245c67ea0d61476e6056102
Lyons II <NRGO061@UPGF.COM>
2018-03-11 23:59:48 -0400

    added functionality to use password and setup job.

--- RebootServerGUII.ps1
+++ RebootServerGUII.ps1
@@ -1,14 +1,18 @@
-Function BuildSched($sname,$reboottime) {
+# Ref: https://tinyurl.com/y8gn44xw
+
+Function BuildSched($sname,$reboottime,$pw) {
     if(Test-WSMan -ComputerName "wpsusa02.upgf.com" -EA 0) {
         write-host $sname
         write-host $reboottime
+        write-host $pw
         Invoke-Command -computername "WPSUSA02" {
-            param($sname)
-            $settings = New-ScheduledTaskSettingsSet -Hidden
-            $trigger = New-ScheduledTaskTrigger -Weekly -at 9am -DaysOfWeek Sunday
+            param($sname,$reboottime,$pw)
+#            $principal = New-ScheduledTaskPrincipal -UserID "RICHMOND\NBCH404" -password "asdf" -LogonType Password -RunLevel Highest
+            $settings = New-ScheduledTaskSettingsSet -Hidden:$false -Compatibility Win8
+            $trigger = New-ScheduledTaskTrigger -Weekly -at $reboottime -DaysOfWeek Sunday
             $action = New-ScheduledTaskAction -Execute "c:\windows\system32\rebootsrv.bat $sname"
-            Register-ScheduledTask -trigger $trigger -TaskName $sname -settings $settings -TaskPath 'UPSReboots' -Action $action -User $user -Password $password
-        } -argumentList $sname
+            Register-ScheduledTask -trigger $trigger -TaskName $sname -settings $settings -TaskPath 'UPSReboots' -Action $action -User "Richmond\nbch404" -Password $pw -Runlevel Highest
+        } -argumentList $sname, $reboottime, $pw
     } else {
         "WPSUSA02 not available"
         Exit
@@ -16,7 +20,7 @@ Function BuildSched($sname,$reboottime) {
 }
 #From: https://poshgui.com/#
 Add-Type -AssemblyName System.Windows.Forms
-$version = 1.09
+$version = 1.11
 $User = ''
 $password = ''
 
@@ -49,12 +53,29 @@ $label_ServerName.location = new-object system.drawing.point(10,20)
 $label_ServerName.Font = "Microsoft Sans Serif,10"
 $form_rebootserver.controls.Add($label_ServerName) 
 
+$textBox_Password = New-Object system.windows.Forms.TextBox 
+$textBox_Password.Text = "NBCH404 UserName"
+$textBox_Password.Width = 100
+$textBox_Password.Height = 10
+$textBox_Password.location = new-object system.drawing.point(103,43)
+$textBox_Password.Font = "Microsoft Sans Serif,10"
+$form_rebootserver.controls.Add($textBox_Password) 
+
+$label_Password = New-Object system.windows.Forms.Label 
+$label_Password.Text = "Password: "
+$label_Password.AutoSize = $true
+$label_Password.Width = 25
+$label_Password.Height = 10
+$label_Password.location = new-object system.drawing.point(10,43)
+$label_Password.Font = "Microsoft Sans Serif,10"
+$form_rebootserver.controls.Add($label_Password) 
+
 $label_time = New-Object system.windows.Forms.Label 
 $label_time.Text = "Time (00:00): "
 $label_time.AutoSize = $true
 $label_time.Width = 25
 $label_time.Height = 10
-$label_time.location = new-object system.drawing.point(10,43)
+$label_time.location = new-object system.drawing.point(10,66)
 $label_time.Font = "Microsoft Sans Serif,10"
 $form_rebootserver.controls.Add($label_time) 
 
@@ -62,7 +83,7 @@ $textBox_time = New-Object system.windows.Forms.TextBox
 $textBox_time.Text = "06:00"
 $textBox_time.Width = 100
 $textBox_time.Height = 43
-$textBox_time.location = new-object system.drawing.point(103,45)
+$textBox_time.location = new-object system.drawing.point(103,66)
 $textBox_time.Font = "Microsoft Sans Serif,10"
 $form_rebootserver.controls.Add($textBox_time) 
 
@@ -71,7 +92,7 @@ $label_day.Text = "Day: "
 $label_day.AutoSize = $true
 $label_day.Width = 25
 $label_day.Height = 10
-$label_day.location = new-object system.drawing.point(11,69)
+$label_day.location = new-object system.drawing.point(11,89)
 $label_day.Font = "Microsoft Sans Serif,10"
 $form_rebootserver.controls.Add($label_day) 
 
@@ -79,7 +100,7 @@ $textBox_Day = New-Object system.windows.Forms.TextBox
 $textBox_Day.Text = "Sunday"
 $textBox_Day.Width = 100
 $textBox_Day.Height = 20
-$textBox_Day.location = new-object system.drawing.point(102,68)
+$textBox_Day.location = new-object system.drawing.point(102,89)
 $textBox_Day.Font = "Microsoft Sans Serif,10"
 $form_rebootserver.controls.Add($textBox_Day) 
 
@@ -87,11 +108,11 @@ $button_ScheduleTask = New-Object system.windows.Forms.Button
 $button_ScheduleTask.Text = "Schedule Task"
 $button_ScheduleTask.Width = 80
 $button_ScheduleTask.Height = 40
-$button_ScheduleTask.location = new-object system.drawing.point(110,109)
+$button_ScheduleTask.location = new-object system.drawing.point(110,112)
 $button_ScheduleTask.Font = "Microsoft Sans Serif,10"
 $button_ScheduleTask.Add_Click({
     #$button_ScheduleTask.Text "Scheduling"textBox_ServerName
-    BuildSched $textBox_ServerName.Text $textBox_time.text
+    BuildSched $textBox_ServerName.Text $textBox_time.text $textBox_password.text
     #$button_ScheduleTask.Text "Scheduled!"
 })
 $form_rebootserver.controls.Add($button_ScheduleTask) 
