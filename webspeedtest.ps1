f8644feebb08c8b52bab56d3f3fff609f2a12f0f
Jack Lyons <jack1729@hotmail.com>
2018-12-20 04:42:05 -0500

    Added Get-UrlStatusCode function

--- webspeedtest.ps1
+++ webspeedtest.ps1
@@ -1,2 +1,23 @@
+function Get-UrlStatusCode([string] $Url)
+{
+    try
+    {
+        (Invoke-WebRequest -Uri $Url -UseBasicParsing -DisableKeepAlive).StatusCode
+    }
+    catch [Net.WebException]
+    {
+        [int]$_.Exception.Response.StatusCode
+    }
+}
+
+
 $wc = New-Object net.webclient; 
-$wc.Proxy = [System.Net.GlobalProxySelection]::GetEmptyWebProxy(); "{0:N2} Mbit/sec" -f ((100/(Measure-Command {$wc.Downloadfile('http://wpadma01/Summer.txt',"c:\upgf\speedtest.test")}).TotalSeconds)*8)
+$url = "http://wpadma01/summer.txt"
+#  Get-URLStatusCode from https://stackoverflow.com/questions/20259251/powershell-script-to-check-the-status-of-a-url
+$statusCode = Get-UrlStatusCode 'httpstat.us/500'
+if ($statusCode -eq 200) {
+    $wc.Proxy = [System.Net.GlobalProxySelection]::GetEmptyWebProxy(); "{0:N2} Mbit/sec" -f ((100/(Measure-Command {$wc.Downloadfile('http://wpadma01/Summer.txt',"c:\upgf\speedtest.test")}).TotalSeconds)*8)
+} else {
+    "$url not reachable: $StatusCode"
+}
+
